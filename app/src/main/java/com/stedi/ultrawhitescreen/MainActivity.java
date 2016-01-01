52cc1ec63a9eb9c387c3c3ee9ecf8cdea2101678
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2016-01-01 16:22:12 +0100

    release v2.0

---
+++ app/src/main/java/com/stedi/ultrawhitescreen/MainActivity.java
@@ -0,0 +1,26 @@
+package com.stedi.ultrawhitescreen;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Window window = getWindow();
+
+        window.getDecorView().setBackgroundColor(Color.WHITE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
+        WindowManager.LayoutParams params = window.getAttributes();
+        params.screenBrightness = 1F;
+        window.setAttributes(params);
+    }
+}
